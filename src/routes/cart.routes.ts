a35e4bd062d85c8a937cd6c3290733c2540d6f25
A2kmoise <abayomoise950@gmail.com>
2025-05-09 07:33:44 +0200

    1.0.5

--- src/routes/cart.routes.ts
+++ src/routes/cart.routes.ts
@@ -6,7 +6,7 @@ import { requireAuth } from '../middleware/auth.middleware';
 const cartRouter = Router();
 
 cartRouter.get('/',requireAuth,cart_get);
-cartRouter.post('/',/*requireAuth,*/ cart_post);
+cartRouter.post('/',requireAuth,cart_post);
 cartRouter.put('/:itemId',requireAuth, cart_put);
 cartRouter.delete('/:itemId',requireAuth, cart_deleteById);
 cartRouter.delete('/',requireAuth, cart_delete);
