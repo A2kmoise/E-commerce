79994bd0467b5d4ddef2d95db5a64fc3bb288aea
A2kmoise <abayomoise950@gmail.com>
2025-05-21 10:33:40 +0200

    1.0.5

--- src/controllers/sms.controllers.ts
+++ src/controllers/sms.controllers.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import { Twilio } from "twilio";
 import { Request, Response } from "express";
+import { sendSMS } from "../services/sms.services";
 
-export const send_sms = ( req:Request, res:Response) => {
-res.send('Sms sent to client');
-}
+export const send_sms = async ( req:Request, res:Response) => {
+  try {
+    await sendSMS(req, res);
+  } catch (error) {
+    res.status(500).json({ error: "An unexpected error occurred.", details: error });
+  }
+};
 
 export const verify_sms = ( req:Request, res:Response) => {
 res.send('Sms verified');
--- src/services/sms.services.ts
+++ src/services/sms.services.ts
@@ -1,2 +1,44 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { Twilio } from 'twilio';
+import dotenv from 'dotenv';
+import { PrismaClient } from '../generated/prisma/client';
+
+dotenv.config();
+const prisma = new PrismaClient();
+
+export const sendSMS = async (req: Request, res: Response) => {
+  const { number } = req.body;
+
+  const accountSid = process.env.ACCOUNT_SID;
+  const authToken = process.env.AUTH_TOKEN;
+  const senderNumber = process.env.SENDER_NUMBER;
+
+  
+  if (!accountSid || !authToken || !senderNumber) {
+    return res.status(500).json({ error: 'Twilio credentials' });
+  }
+
+ 
+  if (!number) {
+    return res.status(400).json({ error: " number is required " });
+  }
+
+  const client = new Twilio(accountSid, authToken);
+
+
+  const predefinedMessage = "Welcome to Bebay! We're excited to have you.";
+
+  try {
+    const message = await client.messages.create({
+      from: senderNumber,
+      to: number,
+      body: predefinedMessage,
+    });
+
+    console.log('Message sent:', message.sid);
+    return res.status(200).json({ message: 'SMS sent successfully', sid: message.sid });
+  } catch (error) {
+    console.error('Failed to send SMS:', error);
+    return res.status(500).json({ error: 'Failed to send SMS', details: error });
+  }
+};
