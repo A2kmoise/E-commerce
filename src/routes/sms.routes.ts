27b279c7b1ef5192a6765bbc5577d53eb1718323
A2kmoise <abayomoise950@gmail.com>
2026-04-22 14:07:14 +0200

    finished ecommerce

--- src/routes/sms.routes.ts
+++ src/routes/sms.routes.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { send_sms, verify_sms } from "../controllers/sms.controllers";
 import { Router } from "express";
+import { requireAuth } from "../middleware/auth.middleware";
 
 const smsRouter = Router();
 
-smsRouter.post('/sms-send', send_sms);
-smsRouter.post('/sms-verify', verify_sms);
+smsRouter.post('/sms-send',requireAuth,send_sms);
+smsRouter.post('/sms-verify',requireAuth, verify_sms);
 
 
 export default smsRouter;
